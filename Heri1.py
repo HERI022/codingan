35e1cb094486f5c9ff407c40faa3b5ecf56485b0
HERI022 <115393209+HERI022@users.noreply.github.com>
2022-10-19 20:31:38 +0800

    Add files via upload

---
+++ Heri1.py
@@ -0,0 +1,9 @@
+nama= "Heri"
+usia= 20
+tinggi_badan= 1.7
+status= False
+
+print ("saya bernama",nama)
+print ("usia saya",usia,"tahun")
+print ("tinggi badan saya",tinggi_badan,"M")
+print ("status pernikahan",status)
