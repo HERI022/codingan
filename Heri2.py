f2579ff75775a39ee7367c22e353990a696ee4d2
HERI022 <115393209+HERI022@users.noreply.github.com>
2022-10-20 20:22:05 +0800

    Add files via upload

---
+++ Heri2.py
@@ -0,0 +1,47 @@
+
+print("=== ARITMATIKA === \n")
+
+#operator_aritmatika
+
+
+#penjumlahan
+angka_1 = 6
+angka_2 = 4
+hasil = angka_1 + angka_2
+print (hasil)
+
+#pengurangan
+angka_1 = 6
+angka_2 = 4
+hasil = angka_1 - angka_2
+print (hasil)
+
+#perkalian
+angka_1 = 6
+angka_2 = 4
+hasil = angka_1 * angka_2
+print (hasil)
+
+#pembagian
+angka_1 = 6
+angka_2 = 4
+hasil = angka_1 / angka_2
+print (hasil)
+
+#Sisa bagi/modulus
+angka_1 = 6
+angka_2 = 4
+hasil = angka_1 % angka_2
+print (hasil)
+
+#pangkat
+angka_1 = 6
+angka_2 = 4
+hasil = angka_1 ** angka_2
+print (hasil)
+
+#pembagian bulan
+angka_1 = 6
+angka_2 = 4
+hasil = angka_1 // angka_2
+print (hasil)
