87898595ebd4e09fada37bb5b99ec79755321535
HERI022 <115393209+HERI022@users.noreply.github.com>
2022-10-21 19:25:50 +0800

    Add files via upload

---
+++ Heri_day3.py
@@ -0,0 +1,24 @@
+print("===", "Operator Aritmatika Menggunakan Input Dari User", "===")
+
+angka1 = int(input("masukkan angka pertama :"))
+angka2 = int(input("masukkan angka kedua :"))
+
+#penjumlahan
+hasil = angka1 + angka2
+print("Hasil dari", angka1, "+", angka2, "=", hasil)
+
+#pengurangan
+hasil = angka1 - angka2
+print("Hasil dari", angka1, "-", angka2, "=", hasil)
+
+#perkalian
+hasil = angka1 * angka2
+print("Hasil dari", angka1, "*", angka2, "=", hasil)
+
+#pembagian
+hasil = angka1 / angka2
+print("Hasil dari", angka1, "/", angka2, "=", hasil)
+
+
+
+
