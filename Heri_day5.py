198add148924a4a9dffbff16c98f8158959a6c21
HERI022 <115393209+HERI022@users.noreply.github.com>
2022-10-23 07:45:09 +0800

    Add files via upload

---
+++ Heri_day5.py
@@ -0,0 +1,41 @@
+print("===" "Operator Penugasan" "===")
+
+#sama dengan (=)
+#tambah sama dengan (+=)
+#kurang sama dengan (-=)
+#kali sama dengan (*=)
+#bagi sama dengan (/=)
+#sisa bagi sama dengan (%=)
+#pangkat sama dengan (**=)
+#pembagian bulat sama dengan (//=)
+
+
+nilai = 2
+nilai += 3
+print(nilai)
+
+nilai2 = 5
+nilai2 =+ 3
+print(nilai2)
+
+nilai -= 2
+print(nilai)
+
+nilai *= 8
+print(nilai)
+
+nilai /= 2
+print(nilai)
+
+nilai %= 5
+print(nilai)
+
+nilai **= 6
+print(nilai)
+
+nilai //= 4
+print(nilai)
+
+
+
+
