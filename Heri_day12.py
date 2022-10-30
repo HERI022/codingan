442055e914b528992da62adfa615cdb710599336
HERI022 <115393209+HERI022@users.noreply.github.com>
2022-10-30 18:41:01 +0800

    Add files via upload

---
+++ Heri_day12.py
@@ -0,0 +1,29 @@
+# date and time
+# Codingan sederhana untuk menghitung umur, hari_lahir.
+# semua itu akan muncul ketika kita menginput tanggal lahir, bulan, dan tahun.
+
+import datetime as dt
+
+print("\n",10*"=","Heri si Tampan dan Pemberani","="*10,"\n")
+print("Silahkan masukkan tanggal, bulan, dan tahun lahir anda !\n")
+
+tanggal = int(input("Tanggal \t:"))
+bulan = int(input("Bulan \t\t:"))
+tahun = int(input("Tahun \t\t:"))
+
+print("\n")
+tanggal_lahir = dt.date(tahun,bulan,tanggal)
+
+print(f"Tanggal lahir anda adalah :{tanggal_lahir}")
+print(f"Anda lahir pada hari :{tanggal_lahir:%A}")
+
+hari_ini = dt.date.today()
+print("\nHari ini tanggal :",hari_ini)
+print(f"sekarang hari :{hari_ini:%A}")
+
+hari = hari_ini - tanggal_lahir
+tahun = hari.days // 365
+bulan = (hari.days % 365) // 30
+
+print("\nAnda sekarang berusia",tahun, "tahun", bulan, "bulan")
+
