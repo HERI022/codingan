922d1960b81b2effa9c0942fd6c11fb9baed388f
HERI022 <115393209+HERI022@users.noreply.github.com>
2022-11-24 22:03:23 +0800

    Add files via upload

---
+++ Heri_day37.py
@@ -0,0 +1,56 @@
+#program mencari hasil persamaan kuadrat
+#menentukan hasil dari x1 dan x2 dari persamaan kuadrat
+#saya mengimpor cmath dari library python
+#modul cmath dengan fungsi sqrt.
+
+selesai = True
+def menu():
+    while selesai == True:
+        print(55*"-")
+        print("\n",10*"=","MENYELESAIKAN PERSAMAAN KUADRAT","="*10,"\n")
+        print(55*"-")
+        print("\n","bentuk umum persamaan kuadrat adalah ax**2 + bx + c = 0")
+
+        import cmath
+
+        z = "(ax**2 + bx + c = 0)"
+
+        a = int(input("masukkan nilai dari a :"))
+        b = int(input("masukkan nilai dari b :"))
+        c = int(input("masukkan nilai dari c :"))
+
+        rumus = int((b**2)-(4*a*c))
+
+        #mencari dan menentukan nilai dari x1 dan x2
+
+        x1 = (-b - cmath.sqrt(rumus))/(2*a)
+        x2 = (-b + cmath.sqrt(rumus))/(2*a)
+
+        print("\nhasil dari x1 adalah",(x1))
+        print("hasil dari x2 adalah",(x2))
+
+        print("\njadi himpunan penyelesaian dari",z)
+        print("x1 =",x1,"dan", "x2 =",x2)
+
+        ulang()
+
+def ulang():
+    while True :
+        print()
+        ulang = input("Lanjut menghitung persamaan kuadrat y/t :")
+
+        if ulang == "y" or ulang == "Y":
+            menu()
+        elif ulang == "t" or ulang == "T":
+            print("TERIMAKASIH")
+            global selesai
+            selesai = False
+            break
+        else:
+            print("masukkan program dengan benar")
+
+    print("\n\nPROGRAM SELESAI")
+
+menu()
+
+
