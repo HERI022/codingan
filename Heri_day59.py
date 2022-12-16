7152a309578bfa9afeb3ea19741e6bc069c9000f
HERI <115393209+HERI022@users.noreply.github.com>
2022-12-16 22:30:12 +0800

    Add files via upload

---
+++ Heri_day59.py
@@ -0,0 +1,16 @@
+class luas:
+    sisi1 = None
+    sisi2 = None
+    
+    
+hasil = luas()
+a = int(input('masukan nilai sisi1 : '))
+b = int(input('mauskan nilai sisi2 : '))
+hasil = a * b
+print('sisi 1 =',a)
+print('sisi 2 =',b)
+print('luas =',hasil)
+print('hasil dari',hasil,'didapatkan dari hasil kali antara',a,'dikali',b)
+
+
+print('BESOK DILENGKAPI LAGI')
