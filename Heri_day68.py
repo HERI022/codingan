2018e11fd1860945ba92178fec370463f8e649db
HERI <115393209+HERI022@users.noreply.github.com>
2022-12-25 21:59:37 +0800

    Add files via upload

---
+++ Heri_day68.py
@@ -0,0 +1,17 @@
+print("=== Program konversi km ke Mil ===")
+print()
+
+
+# Menginput Jarak dalam Satuan Kilometer
+kilometer = float(input("Tuliskan Jarak dalam Kilometer: "))
+
+# Nilai konversi
+konversi = 0.621371
+
+# Rumus Konversi
+mil = kilometer * konversi
+
+# Menampilkan Hasil Konversi Jarak
+print(kilometer, "Kilometer sama dengan", mil,'Mil')
+
+print("Besok saya lengkapi lagi bang")
