62dc7fb9b53ba8a72dde43843affee40251f7919
HERI <115393209+HERI022@users.noreply.github.com>
2022-12-28 22:31:46 +0800

    Add files via upload

---
+++ Heri_day71.py
@@ -0,0 +1,19 @@
+# Gaji pokok masukkan melalui input
+#Gaji lembur/jam adalah Rp. 55.000
+#Lama lembur lembur dimasukkan melalui fungsi input()
+gaji_kotor= int(input("Masukkan gaji kotor :"))
+
+print("Gaji lembur/jam adalah Rp.55.000")
+
+lembur= int(input("Masukkan lama lembur (jam) :"))
+
+print("Gaji Kotor \t:Rp.",gaji_kotor)
+print("Lembur (jam)\t:",lembur, "jam")
+
+gajiLembur = lembur * 55000
+
+print("Gaji lembur\t:Rp.",gajiLembur)
+
+hasil=gaji_kotor + gajiLembur
+
+print("gaji bersih\t:Rp.",hasil)
