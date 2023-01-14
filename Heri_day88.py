e763f23ac4e5a5874a3548255480f4422840afeb
HERI <115393209+HERI022@users.noreply.github.com>
2023-01-14 21:37:57 +0800

    Add files via upload

---
+++ Heri_day88.py
@@ -0,0 +1,27 @@
+# is = Bernilai True jika kedua operand merujuk ke object yang sama dan berisi nilai yang sama.
+
+# is not = Bernilai True jika kedua operand merujuk ke object yang tidak sama.
+
+print(33*"=")
+print("Contoh Program Operator Identitas")
+print(33*"=")
+print()
+
+a = 5
+b = 5
+c = 6
+print('a is b :', a is b)
+print('a is c :', a is c)
+print('a is not c :', a is not c)
+print('\n')
+  
+i = 'syaputrahery581@gmail.com'
+j = 'syaputrahery581@gmail.com'
+print('i is j :', i is j)
+print('i is not j :', i is not j)
+print('\n');
+  
+x = ['a','b','c']
+y = ['a','b','c']
+print('x is y :', x is y)
+print('x is not y :', x is not y)
