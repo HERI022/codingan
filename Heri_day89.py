af2c0f80ba39a1fc7adc67700e1cbc63fa904e33
HERI <115393209+HERI022@users.noreply.github.com>
2023-01-15 19:24:36 +0800

    Add files via upload

---
+++ Heri_day89.py
@@ -0,0 +1,28 @@
+# in = Bernilai True jika nilai yang dicari ada di dalam himpunan
+
+# not in = Bernilai True jika nilai yang dicari tidak ada dalam himpunan
+
+print(43*"=")
+print('=== CONTOH PROGRAM OPERATOR KEANGGOTAAN ===')
+print(43*"=")
+
+foo = 'Heri'
+print('foo :',foo)
+print('\'i\' in foo     :', 'i' in foo)
+print('\'k\' not in foo :', 'k' not in foo)
+print('\'d\' not in foo :', 'd' not in foo)
+print('\n')
+ 
+ 
+bar = ['a','b','c']
+print('bar :',bar)
+print('\'a\' in bar     :', 'a' in bar)
+print('\'a\' not in bar :', 'a' not in bar)
+print('\'d\' not in bar :', 'd' not in bar)
+print('\n')
+ 
+baz = (22,12,2001,55)
+print('baz :',baz)
+print('2001 in baz     :', 2001 in baz)
+print('2001 not in baz :', 2001 not in baz)
+print('35 not in baz  :', 35 in baz)
