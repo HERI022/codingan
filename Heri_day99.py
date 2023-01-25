a014b6990c6651e9bbe86f59b105554597f4b173
HERI <115393209+HERI022@users.noreply.github.com>
2023-01-25 22:55:11 +0800

    Add files via upload

---
+++ Heri_day99.py
@@ -0,0 +1,45 @@
+print ('NAMA : Heri')
+print ('NIM  : D0222544')
+print ('Fakultas Teknik Jurusan Teknik Informatika')
+print ('UNIVERSITAS SULAWESI BARAT')
+print (' ')
+print ('\t=== CONTOH PROGRAM TERBILANG ===')
+print (50*'-','\n')
+
+def Terbilang(bil):
+    angka = ["","Satu","Dua","Tiga","Empat","Lima","Enam",
+             "Tujuh","Delapan","Sembilan","Sepuluh","Sebelas"]
+    Hasil = " "
+    n = int(bil)
+    if n>= 0 and n <= 11:
+        Hasil = angka[n]
+    elif n <20:
+        Hasil = Terbilang (n-10) + " Belas "
+    elif n <100:
+        Hasil = Terbilang (n/10) + " Puluh " + Terbilang (n%10)
+    elif n <200:
+        Hasil = " Seratus " + Terbilang (n-100)
+    elif n <1000:
+        Hasil = Terbilang (n/100) + " Ratus " + Terbilang (n%100)
+    elif n <2000:
+        Hasil = " Seribu " + Terbilang (n-1000)
+    elif n <1000000:
+        Hasil = Terbilang (n/1000) + " Ribu " + Terbilang (n%1000)
+    elif n <1000000000:
+        Hasil = Terbilang (n/1000000) + " Juta " + Terbilang (n%1000000)
+    elif n <1000000000000:
+        Hasil = Terbilang (n/1000000000) + " Milyar " + Terbilang (n%1000000000)
+    elif n <1000000000000000:
+        Hasil = Terbilang (n/1000000000000) + " Triliyun " + Terbilang (n%1000000000000)
+    elif n == 1000000000000000:
+        Hasil = "Satu Kuadriliun"
+    else:
+        Hasil = "Angka Hanya Sampai Satu Kuadriliun"
+
+    return Hasil
+a=1
+while a != '0' :
+    a = input('Masukan angka dari 1 sd 1.000.000.000.000.000(0 untuk keluar)\nMasukan Angka : ')
+    huruf = Terbilang(a)
+    print (' Terbilang ---- '+huruf+ ' Rupiah ----')
+    continue
